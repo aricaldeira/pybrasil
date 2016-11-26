71b0fa0212b8b5807cfa2f60a046031699d9b25b
Luis Felipe Mileo <mileo@kmee.com.br>
2016-11-26 19:08:19 -0200

    [ADD] setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pybrasil',
+    version='0.0.0.1',
+    packages=['', 'ncm', 'base', 'data', 'ibge', 'valor', 'feriado', 'produto',
+              'telefone', 'inscricao'],
+    url='https://github.com/aricaldeira/pybrasil',
+    license='',
+    author='Aristides Caldeira',
+    author_email='aristides.caldeira@tauga.com.br',
+    description=''
+)
