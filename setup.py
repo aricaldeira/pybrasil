d32204982f86909268a28a8be0f29cb2d27f2342
Luis Felipe Mileo <mileo@kmee.com.br>
2016-11-26 19:19:54 -0200

    [REF] Moving files to pybrasil module

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.core import setup
 setup(
     name='pybrasil',
     version='0.0.0.1',
-    packages=['', 'ncm', 'base', 'data', 'ibge', 'valor', 'feriado', 'produto',
-              'telefone', 'inscricao'],
-    url='https://github.com/aricaldeira/pybrasil',
+    packages=['', 'pybrasil', 'pybrasil.ncm', 'pybrasil.base', 'pybrasil.data',
+              'pybrasil.ibge', 'pybrasil.valor', 'pybrasil.feriado',
+              'pybrasil.produto', 'pybrasil.telefone', 'pybrasil.inscricao'],
+    url='git@github.com:aricaldeira/pybrasil.git',
     license='',
     author='Aristides Caldeira',
     author_email='aristides.caldeira@tauga.com.br',
