17ed084e084afcb67740b1943a7babdcbaa91135
Ari Caldeira <ari.caldeira@tauga.com.br>
2017-10-27 10:30:59 -0200

    [FEA] Ativando locale

--- pybrasil/__init__.py
+++ pybrasil/__init__.py
@@ -7,3 +7,13 @@ from __future__ import (division, print_function, unicode_literals,
 from . import (base, data, ibge, inscricao, ncm, telefone, valor, template,
                febraban)
 from .python_pt_BR import python_pt_BR
+
+import sys
+import locale
+
+if sys.version_info.major == 2:
+    locale.setlocale(locale.LC_ALL, b'pt_BR.UTF-8')
+    locale.setlocale(locale.LC_COLLATE, b'pt_BR.UTF-8')
+else:
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+    locale.setlocale(locale.LC_COLLATE, 'pt_BR.UTF-8')
