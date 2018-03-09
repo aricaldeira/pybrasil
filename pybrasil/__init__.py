292034736bb13b1b18924e0808b59958cb676143
Ari Caldeira <ari.caldeira@tauga.com.br>
2018-03-09 11:15:09 -0300

    [FEA] Ativando código de barras

--- pybrasil/__init__.py
+++ pybrasil/__init__.py
@@ -5,7 +5,7 @@ from __future__ import (division, print_function, unicode_literals,
 
 
 from . import (base, data, ibge, inscricao, ncm, telefone, valor, template,
-               febraban, certificado)
+               febraban, certificado, codigo_barras)
 from .python_pt_BR import python_pt_BR
 
 import sys
